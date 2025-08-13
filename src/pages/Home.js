03466c270777b3486ee0f475d36ba677d4cfab5f
Peter <peter_s_96@live.com>
2025-08-12 23:38:46 -0700

    added animation for home-page

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -4,8 +4,10 @@ import "./Home.css";
 const Home = () => {
   return (
     <div className="home-container home-page">
-      <h2>Welcome to Mokkoji Northridge</h2>
-      <p>"Mokkoji" 모꼬지 refers to a gathering or feast where people come together to enjoy food and drinks</p>
+      <h2 className="animate-text">Welcome to Mokkoji Northridge</h2>
+      <p className="animate-text">
+        "Mokkoji" 모꼬지 refers to a gathering or feast where people come together to enjoy food and drinks
+      </p>
     </div>
   );
 };
